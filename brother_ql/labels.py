f765ebef1202a719814302763f56d3359a815264
Matt Sowden <549624+resba@users.noreply.github.com>
2023-02-04 18:11:48 -0500

    Add 360ppi version of PT24mm label

--- brother_ql/labels.py
+++ brother_ql/labels.py
@@ -105,6 +105,7 @@ ALL_LABELS = (
   Label("d24",    ( 24,  24), FormFactor.ROUND_DIE_CUT, ( 284,  284), ( 236,  236),  42 ),
   Label("d58",    ( 58,  58), FormFactor.ROUND_DIE_CUT, ( 688,  688), ( 618,  618),  51 ),
   Label("pt24",   ( 24,   0), FormFactor.PTOUCH_ENDLESS,( 128,    0), ( 128,    0),   0, feed_margin=14),
+  Label("pt24-360", ( 24, 0), FormFactor.PTOUCH_ENDLESS, ( 256, 0), ( 234, 0), 171 , feed_margin=14, restricted_to_models=['PT-P950NW']),
 )
 
 class LabelsManager(ElementsManager):
